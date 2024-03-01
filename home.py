602ff7c0fe91cdc7df33ba5bc7c301993aca3c62
Ebrahim Tayabali <ebrahimhakimuddin@gmail.com>
2024-03-01 20:03:43 +0530

    Version 1

--- home.py
+++ home.py
@@ -1,6 +1,29 @@
-from flask import Flask, render_template
+from flask import *
+import random
 
 app = Flask(__name__)
 @app.route("/")
-def show():
+@app.route("/home")
+def homepage():
     return render_template("index.html")
+
+@app.route("/RandomNumberGenerator", methods=["GET", "POST"])
+def RnGen():
+    if request.method == "POST":
+        number = int(request.form.get("number"))
+        result = randomize(number)
+        number - 1
+        return result
+        # return render_template("RandomNumberGenerator.html")
+    return render_template("RandomNumberGenerator.html")
+
+def randomize(number):
+    result = random.randrange(1, number)
+    return str(result)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
